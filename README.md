384a6049450aa5055886e3cc8b174f3c140881c3
CrystalKSTW <crystalkstw372000@gmail.com>
2022-06-04 20:33:26 +0630

    fibonacci.rb has finished and wrote some info in README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Project-Recursion
+
+## Exercise from The Odin Project
+
+## Fibonacci.rb
+- Includes 2 methods 
+1. Using Iteration
+2. By Recursion
---
+++ lib/fibonacci.rb
@@ -0,0 +1,20 @@
+#using iteration
+
+def fibs(n, result = [0,1])
+    for i in 2..n
+        result << result[i-1] + result[i-2]
+    end
+    result
+end
+
+p fibs(8)
+
+#recursively
+
+def fib_rec(n)
+    return [0,1] if n < 2
+    arr = fib_rec(n-1)
+    arr << arr[-2] + arr[-1]
+end
+
+p fib_rec(8)
